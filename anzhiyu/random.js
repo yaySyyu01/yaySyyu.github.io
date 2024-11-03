a26ee662534f13e7a75b43fe8baa6defec550567
yaySyyu01 <starthu337@gmail.com>
2024-11-04 02:45:17 +0800

    Site updated: 2024-11-04 02:45:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/04/hello-world/"];function toRandomPost(){
+var posts=["2024/11/04/ceshi/","2024/11/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
